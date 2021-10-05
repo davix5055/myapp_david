45ad3c4ae37313df28190d939af8a1501408da7b
davix5055 <wubao85@gmail.com>
2021-10-05 15:57:58 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+   super(props)
+
+    this.state={
+
+      color:"blue"
+      
+   }
+   this.cambiarColor = this.cambiarColor.bind(this);
+
+   
+
+  }
+  cambiarColor(){
+
+   if(this.state.color == "blue"){
+
+    this.setState({color:"orange"})
+
+   }
+   
+   else{
+
+    this.setState({color:"blue"})
+
+
+   }
+    
+
+   }
+  
+    render(){
+    return(
+      <div>
+
+     <button onClick={this.cambiarColor} style={{backgroundColor: this.state.color}}>
+
+      My button
+
+    </button>
+
+
+      </div>
+    )
+  }
 }
 
 export default App;
